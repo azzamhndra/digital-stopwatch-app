4a3ab746ccbe446a7faf92eb66e11b0018831a4d
Azza Mahendra <176836552+azzamhndra@users.noreply.github.com>
2025-11-29 13:58:54 +0700

    feat: add stopwatch UI layout

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,7 +1,20 @@
 import { useState, useEffect, useRef } from 'react';
 
 const Stopwatch = () => {
-  return <div></div>;
+  return (
+    <div className="stopwatch-container">
+      <p className="stopwatch-title">STOPWATCH</p>
+      <p className="stopwatch-display">00:00:00</p>
+      <div className="stopwatch-controls">
+        <button className="stopwatch-startstop">
+          <i class="ri-play-fill"></i>
+        </button>
+        <button className="stopwatch-reset">
+          <i class="ri-reset-right-line"></i>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Stopwatch;
